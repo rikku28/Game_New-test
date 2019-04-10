82c24f10ca09c87b18b047b0406fc194c77184d3
Amé G.M <45341378+rikku28@users.noreply.github.com>
2019-04-10 21:29:21 +0200

    Update check-login.js
    Options de la regex mises entre ' ' .

--- config/check-login.js
+++ config/check-login.js
@@ -6,7 +6,7 @@ exports.log = console.log();
 
 // Expression régulière pour vérifier l'url de l'avatar
 // const urlRegex = new RegExp('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$', ig);
-exports.urlRegex = new RegExp('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$', ig);
+exports.urlRegex = new RegExp('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$', 'ig');
 
 // Fonction pour vérifier que le pseudo n'est pas vide
 // let verifPseudo = function(pseudo){
